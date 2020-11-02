a860aba0aff8b97466c875bcf016b6c5cf36a690
Jafea7 <73450040+Jafea7@users.noreply.github.com>
2020-11-02 19:59:35 +1100

    Update start.sh

--- rootfs/app/start.sh
+++ rootfs/app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 cd /app
 
-java -Xmx256m -cp ctbrec-server-3.10.4-final.jar -Dctbrec.config.dir=/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer
+java -Xmx256m -cp ctbrec-server-3.10.4-final.jar -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer
